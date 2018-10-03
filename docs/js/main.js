b23f4e86fed1d04054bd36341b3337c72ea8f955
EbichanOO <takanori.sao@icloud.com>
2018-10-04 02:53:56 +0900

    test_smartphone

--- docs/js/main.js
+++ docs/js/main.js
@@ -10,19 +10,30 @@ var dly = 0; // 次の文章までの待ち時間
 var count = 0;
 var tx = [];
 var txCount = [];
- 
+
 window.onload = function(){
+  /* ----- phone or pc -----*/
+  if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+      // スマホ時に行う処理を書く（文字の大きさを変える処理を書く）
+      for(n=0;n<id.length;n++){
+        var temp = document.getElementById(id[n]);
+        temp.className = 'foo-after';
+      }
+  } else {
+      // PC・タブレット時に行う処理を書く
+      /* 特にない */
+  }
   kamikakushi();
   countSet();
   itimozi();
 }
- 
+
 function countSet(){ // 文字数カウントの初期設定
   for(n=0;n<id.length;n++){
     txCount[n] = 0;
   }
 }
- 
+
 function kamikakushi(){ // 要素をtx[i]に保持させ、非表示にする
   for(i=0;i<id.length;i++){
     id[i] = document.getElementById(id[i]);
@@ -31,7 +42,7 @@ console.log(tx[i]);
     id[i].innerHTML = '';
   }
 }
- 
+
 function itimozi(){ //　一文字ずつ表示させる
  id[count].innerHTML = (tx[count].substr( 0, ++txCount[count] )+"_").replace(/%n/g,"<br>").replace(/%/g,""); // テキストの指定した数の間の要素を表示
  if(tx[count].length != txCount[count]){ // Count が初期の文字列の文字数と同じになるまでループ
