62724b2df8da4d28d50e6a1ec11e107624fbd92f
Unknown <b1017055@fun.ac.jp>
2018-10-03 16:16:18 +0900

    とりあえず動くよ

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,5 @@
 ﻿/* ----- option ----- */
-var id = ['a','b','c']; //指定するidを全て配列で渡す
+var id = ['a','b','c','d']; //指定するidを全て配列で渡す
 var txSp = 100; // テキストの表示速度
 var dly = 0; // 次の文章までの待ち時間
 /* ----- option ----- */
@@ -22,18 +22,18 @@ function countSet(){ // 文字数カウントの初期設定
 function kamikakushi(){ // 要素をtx[i]に保持させ、非表示にする
   for(i=0;i<id.length;i++){
     id[i] = document.getElementById(id[i]);
-    tx[i] = id[i].firstChild.nodeValue.replace("\\n","<br>"); // 初期の文字列
+    tx[i] = id[i].firstChild.nodeValue; // 初期の文字列
 console.log(tx[i]);
     id[i].innerHTML = '';
   }
 }
  
 function itimozi(){ //　一文字ずつ表示させる
- id[count].innerHTML = (tx[count].substr( 0, ++txCount[count] )+"_"); // テキストの指定した数の間の要素を表示
+ id[count].innerHTML = (tx[count].substr( 0, ++txCount[count] )+"_").replace(/%n/g,"<br>").replace(/%/g,""); // テキストの指定した数の間の要素を表示
  if(tx[count].length != txCount[count]){ // Count が初期の文字列の文字数と同じになるまでループ
   setTimeout("itimozi();",txSp); // 次の文字へ進む
  }else{
-  id[count].innerHTML = tx[count].substr( 0, ++txCount[count] ); // テキストの指定した数の間の要素を表示
+  id[count].innerHTML = tx[count].substr( 0, ++txCount[count]).replace(/%n/g,"<br>"); // テキストの指定した数の間の要素を表示
   count++; // 次の段落に進む為のカウントアップ
   if(count != id.length){ // id数が最後なら終了
    setTimeout("itimozi();",dly); // 次の段落へ進む
